dfb3a975307c3453a2196a9400d6dc0cbe754fa7
Mennamostafa14 <mennamo142005@gmail.com>
2026-04-17 21:40:34 +0200

    modified:   src/helpers/__init__.py

---
+++ src/helpers/__init__.py
@@ -0,0 +1,28 @@
+# helpers/__init__.py
+# Public re-exports so callers can do:
+#   from helpers import clean_text, detect_section, ...
+from .text_cleaner import clean_text
+from .section_detector import (
+    SectionSpec,
+    SECTION_REGISTRY,
+    SECTION_WEIGHTS,
+    DEFAULT_SECTION,
+    detect_section,
+    group_lines_by_section,
+)
+from .file_loader import load_from_bytes
+from .chunking import chunk_section_docs
+from .metadata import enrich_chunk_metadata
+
+__all__ = [
+    "clean_text",
+    "SectionSpec",
+    "SECTION_REGISTRY",
+    "SECTION_WEIGHTS",
+    "DEFAULT_SECTION",
+    "detect_section",
+    "group_lines_by_section",
+    "load_from_bytes",
+    "chunk_section_docs",
+    "enrich_chunk_metadata",
+]
